eaab2de395c50351946934e5239c672c2daeb6e6
hima <hima@DESKTOP-8A8ERDV>
2018-10-05 10:30:21 +0530

    hello java

---
+++ magento/src/magento/Demo.java
@@ -0,0 +1,13 @@
+package magento;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	WebDriver driver=new FirefoxDriver();
+    driver.get("https://www.magento.com");
+	}
+
+}
